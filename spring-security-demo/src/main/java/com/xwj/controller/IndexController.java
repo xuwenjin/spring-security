653a0d4ece5eeb88407825d961e28f116fcde310
xuwenjin <563210736@qq.com>
2018-11-30 09:51:21 +0800

    修改SpringSecurity相关功能demo

--- spring-security-demo/src/main/java/com/xwj/controller/IndexController.java
+++ spring-security-demo/src/main/java/com/xwj/controller/IndexController.java
@@ -1,5 +1,6 @@
 package com.xwj.controller;
 
+import org.apache.tomcat.util.net.openssl.ciphers.Authentication;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -10,11 +11,27 @@ import com.xwj.entity.User;
 import com.xwj.service.IUserService;
 
 @RestController
-@RequestMapping("index")
+@RequestMapping
 public class IndexController {
 
 	@Autowired
 	private IUserService userService;
+
+	/**
+	 * 不需要认证的请求
+	 */
+	@GetMapping("/noAuth")
+	public String noAuth() {
+		return "noAuth";
+	}
+
+	/**
+	 * 获取认证信息
+	 */
+	@GetMapping("/me")
+	public Authentication authentication(Authentication authentication) {
+		return authentication;
+	}
 
 	@GetMapping("/find/{id}")
 	public User findById(@PathVariable Long id) {

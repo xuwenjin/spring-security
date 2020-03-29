b26d157420243b4f4c741883aed67fe18966465b
xuwenjin <563210736@qq.com>
2020-03-29 14:58:49 +0800

    修改注释

--- spring-security-browser/src/main/java/com/xwj/config/SecurityConfig.java
+++ spring-security-browser/src/main/java/com/xwj/config/SecurityConfig.java
@@ -42,28 +42,27 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
 
 	@Autowired
 	private SpringSocialConfigurer socialSecurityConfig;
-	
+
 	@Override
 	protected void configure(HttpSecurity http) throws Exception {
 		BrowserProperty browser = securityProperty.getBrowser();
-		
+
 		this.applyPasswordAuthenticationConfig(http);
 
-		http.apply(socialSecurityConfig) //社交配置
-				.and()
-			.rememberMe().userDetailsService(myUserDetailServiceImpl) // 设置userDetailsService
+		http.apply(socialSecurityConfig) // 社交配置
+				.and().rememberMe().userDetailsService(myUserDetailServiceImpl) // 设置userDetailsService
 				.tokenRepository(persistentTokenRepository()) // 设置数据访问层
 				.tokenValiditySeconds(browser.getRememberMeTime()) // 记住我的时间(秒)
-				.and()
-			.authorizeRequests() // 对请求授权
-				.antMatchers(SecurityConst.AUTH_REQUIRE, browser.getLoginPage()
-						, browser.getSignUpUrl(), 
-						"/user/regist") ////处理社交注册请求
+				.and().authorizeRequests() // 对请求授权
+
+				.antMatchers(SecurityConst.AUTH_REQUIRE, browser.getLoginPage(), browser.getSignUpUrl(), "/user/regist") //// 处理社交注册请求
 				.permitAll() // 允许所有人访问login.html和自定义的登录页
-				.anyRequest() // 任何请求
-				.authenticated()// 需要身份认证
+
+				.anyRequest().authenticated() // 任何请求，需要身份认证
+
 				.and().csrf().disable() // 关闭跨站伪造保护
-				.cors() //开启跨域
+
+				.cors() // 开启跨域
 		;
 	}
 
@@ -71,10 +70,8 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
 		// formLogin()是默认的登录表单页，如果不配置 loginPage(url)，则使用 spring security
 		// 默认的登录页，如果配置了 loginPage()则使用自定义的登录页
 		http.formLogin() // 表单登录
-				.loginPage(SecurityConst.AUTH_REQUIRE)
-				.loginProcessingUrl(SecurityConst.AUTH_FORM) // 登录请求拦截的url,也就是form表单提交时指定的action
-				.successHandler(loginSuccessHandler)
-				.failureHandler(loginFailureHandler).and();
+				.loginPage(SecurityConst.AUTH_REQUIRE).loginProcessingUrl(SecurityConst.AUTH_FORM) // 登录请求拦截的url,也就是form表单提交时指定的action
+				.successHandler(loginSuccessHandler).failureHandler(loginFailureHandler).and();
 	}
 
 	/**

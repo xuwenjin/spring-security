cfb12eea17240f1e8cd06e99708fe78539c43795
xuwenjin <563210736@qq.com>
2020-04-14 10:19:45 +0800

    删除多余密码加密配置类

--- spring-security-demo/src/main/java/com/xwj/controller/IndexController.java
+++ spring-security-demo/src/main/java/com/xwj/controller/IndexController.java
@@ -1,23 +1,14 @@
 package com.xwj.controller;
 
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import com.xwj.properties.SecurityProperty;
-
-import io.jsonwebtoken.Claims;
-import io.jsonwebtoken.Jwts;
-
 @RestController
 @RequestMapping("index")
 public class IndexController {
-
-	@Autowired
-	private SecurityProperty securityProperty;
 
 	/**
 	 * 获取资源
@@ -25,16 +16,6 @@ public class IndexController {
 	@GetMapping("/getResource")
 	public String getResource() {
 		return "OK";
-	}
-
-	/**
-	 * 解析jwt
-	 */
-	@GetMapping("/jwt/parse")
-	public Claims parseJwtToken() {
-		String token = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJleHAiOjE1ODU2MzUyODgsInVzZXJfbmFtZSI6Inh3aiIsImF1dGhvcml0aWVzIjpbImFkbWluIiwiUk9MRV9VU0VSIl0sImp0aSI6IjJlZjU3ZmFjLTEwOTYtNGFjYi04ZmNhLTE1NDQzMTViYTA2ZiIsImNsaWVudF9pZCI6InRlc3QiLCJzY29wZSI6WyJyZWFkIl19.vuPRShOdls7sAAMW85qns-3475g25nlA8g9mpXntFrU";
-		return Jwts.parser().setSigningKey(securityProperty.getOauth2().getJwtSignKey()).parseClaimsJws(token)
-				.getBody();
 	}
 
 	/**
